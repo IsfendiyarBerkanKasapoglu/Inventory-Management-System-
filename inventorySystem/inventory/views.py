82efdc667040d4639fb9211b538a3e0513da2042
IsfendiyarBerkanKasapoglu <berkankasapoglu03@hotmail.com>
2022-08-26 17:07:25 +0300

    first commit

---
+++ inventorySystem/inventory/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        "title": "Index page"
+    }
+    return render(request, "inventory/index.html", context=context)
+
+ 
