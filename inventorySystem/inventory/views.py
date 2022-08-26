895e5b1e588785150d9d44ec79afc35c17e2b69f
IsfendiyarBerkanKasapoglu <berkankasapoglu03@hotmail.com>
2022-08-26 17:36:00 +0300

    inventory_system and login.html added

--- inventorySystem/inventory/views.py
+++ inventorySystem/inventory/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Index page"
+        "title": "Index page "
     }
     return render(request, "inventory/index.html", context=context)
 
